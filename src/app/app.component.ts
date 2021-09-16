f37bebce98567bab395e92f89c87a92868e545e1
jnvalle <jnvalle@minsait.com>
2021-09-16 17:25:43 +0200

    componente titulo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'demo-jnvalle';
+  public title = 'App demo de Jnvalle';
+  public get cantidad(): number {
+    return 2;
+  }
+
+  public get precio(): number {
+    return 9;
+  }
 }
---
+++ src/app/titulo/titulo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-titulo',
+  templateUrl: './titulo.component.html',
+  styleUrls: ['./titulo.component.scss']
+})
+export class TituloComponent {
+  public title = 'App demo de Jnvalle';
+  constructor() {}
+
+}
